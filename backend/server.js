ab355ceb244444b3fbd9c2fe197e602686e470cf
Mitesh jain <jainmitesh2393@gmail.com>
2024-09-01 11:54:07 +0530

    Initial commit with auth

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
+});
