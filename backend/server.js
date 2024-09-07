35375c288c536d85f24c65c3832a74bc2b502f70
Mitesh jain <jainmitesh2393@gmail.com>
2024-09-07 17:39:12 +0530

    order and payment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 dotenv.config();
@@ -18,9 +19,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
-
+app.use("/api/payments", paymentRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
