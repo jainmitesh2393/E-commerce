9c98e8af66fc7cb3e9a3d0f0ffc59405bc97b4f4
Mitesh jain <jainmitesh2393@gmail.com>
2024-09-07 17:54:18 +0530

    sales analytics part

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 dotenv.config();
@@ -20,6 +21,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
