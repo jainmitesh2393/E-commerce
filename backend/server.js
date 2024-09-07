f9ddcb1630d0a5c634d3ec785d6e2a7d90a71813
Mitesh jain <jainmitesh2393@gmail.com>
2024-09-07 17:00:08 +0530

    coupon route and controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 dotenv.config();
@@ -15,7 +16,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/cary", cartRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 
 app.listen(process.env.PORT, () => {
